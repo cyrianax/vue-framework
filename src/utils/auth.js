fe4a69b5ec9fb34710070b8086d524d16dbc1663
wangzhong <excella@yeah.net>
2021-02-18 16:20:27 +0800

    add

---
+++ src/utils/auth.js
@@ -0,0 +1,28 @@
+import storage from './storage'
+
+const publicList = [
+  '/common/sign-in'
+]
+
+export default {
+  checkToken (path) {
+    return storage.token
+      ? publicList.indexOf(path) < 0 ? true : { path: '/' }
+      :
+  },
+  checkPermission () {
+
+  },
+  async verify (to) {
+    if (storage.token) {
+      return publicList.indexOf(to.path) < 0 ? true : { path: '/' }
+    } else {
+      return (publicList.indexOf(to.path) < 0) ? {
+        path: '/common/sign-in',
+        query: {
+          redirect: to.path
+        }
+      } : true
+    }
+  }
+}
